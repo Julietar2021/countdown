be73faa8aa948c14ece1189185f29f1f859f295f
Julietar2021 <88993062+Julietar2021@users.noreply.github.com>
2021-09-17 03:27:38 +0200

    Add newyear countdown

---
+++ styles.js
@@ -0,0 +1,37 @@
+
+
+const nextYear = new Date('January 1, 2022 00:00:00').getTime();
+
+function newYear() {
+
+  const toDay = new Date().getTime();
+space = nextYear - toDay; 
+
+
+const sec = 1000;
+const min = sec * 60;
+const hour = min * 60;
+const day = hour * 24;
+
+
+const d= Math.floor(space / (day));
+const h = Math.floor((space % (day)) / (hour));
+const m = Math.floor(( space % (hour)) / (min));
+const s = Math.floor ((space % (min)) / (sec));
+
+
+
+ document.getElementById("seconds").innerText = s;
+document.getElementById("minutes").innerText = m;
+ document.getElementById("hours").innerText = h;
+document.getElementById("days").innerText = d;
+
+
+}
+
+setInterval(function()
+{
+
+newYear();
+}
+)
